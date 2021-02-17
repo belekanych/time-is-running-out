7348c66cbf5f01ccdbb8a37529fbfd53b3f8b4c4
Yurii Belekanych <yuriy.belekanych@gmail.com>
2021-02-18 00:54:57 +0200

    Installed Tailwindcss.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="text-center">
+    <img alt="Vue logo" src="./assets/logo.png" class="inline-block">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
@@ -19,7 +21,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
